e60929ad833d3577db1126717c2b36c3a72492d9
Rahn <zaffrahn@gmail.com>
2016-05-17 14:53:09 +0800

    Topological-Sort.py added

---
+++ chap-5/Topological-Sort.py
@@ -0,0 +1,41 @@
+def topological_sorting(graph):
+	in_degree = [0] * len(graph)
+	for item in graph:
+		for out_vertex in item:
+			in_degree[out_vertex] += 1
+
+	next_list = []
+	for i in range(len(in_degree)):
+		if in_degree[i] == 0:
+			next_list += [i]
+
+	topological_order = []
+	while next_list:
+		new_item  = next_list[0]
+		next_list = next_list[1:]
+		topological_order += [new_item];
+		for index in graph[new_item]:
+			in_degree[index] -= 1
+			if in_degree[index] == 0:
+				next_list += [index]
+
+	return topological_order
+
+adjacency_list = [
+	[2],
+	[3],
+	[3, 4],
+	[5],
+	[5],
+	[6, 10],
+	[7],
+	[12],
+	[9],
+	[10],
+	[11],
+	[12],
+	[13],
+	[]
+]
+
+print(topological_sorting(adjacency_list))
