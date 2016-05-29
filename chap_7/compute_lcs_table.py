156db116c5e93e3d8abec0c5426ebfbe3c04b194
Rahn <zaffrahn@gmail.com>
2016-05-29 17:50:42 +0800

    compute_lcs_table.py modified

--- chap_7/compute_lcs_table.py
+++ chap_7/compute_lcs_table.py
@@ -3,9 +3,9 @@ def compute_lcs_table(x_str, y_str):
 	y_len = len(y_str)
 	lcs_table = {}
 
-	for i in range(x_len):
+	for i in range(-1, x_len):
 		lcs_table[i, -1] = 0
-	for k in range(y_len):
+	for k in range(-1, y_len):
 		lcs_table[-1, k] = 0
 
 	for i in range(x_len):
@@ -17,19 +17,21 @@ def compute_lcs_table(x_str, y_str):
 	return lcs_table
 
 def print_table(table, x_str, y_str):
-	print(end="  ")
+	print(end="    ")
 	for char in y_str:
 		print(char, end=" ")
 	print()
 
-	for i in range(len(x_str)):
-		print(x_str[i], end=" ")
-		for k in range(len(y_str)):
+	for i in range(-1, len(x_str)):
+		if i >= 0:
+			print(x_str[i], end=" ")
+		else:
+			print(end="  ")
+		for k in range(-1, len(y_str)):
 			print(table[i, k], end=" ")
 		print()
 
 if __name__ == '__main__':
 	x_str = "CATCGA"
 	y_str = "GTACCGTCA"
-
 	print_table(compute_lcs_table(x_str, y_str), x_str, y_str)
