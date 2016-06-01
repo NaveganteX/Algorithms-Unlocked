cbeacba5932faa6c45a1c7ac71505181104aef5b
Rahn <zaffrahn@gmail.com>
2016-06-01 19:28:46 +0800

    fa_string_matcher.py revised

--- chap_7/fa_string_matcher.py
+++ chap_7/fa_string_matcher.py
@@ -1,5 +1,4 @@
 def fa_string_matcher(text, next_state, m):
-	print("raw text:", text)
 	state = 0
 	for i in range(len(text)):
 		state = next_state[state, text[i]]
@@ -8,21 +7,24 @@ def fa_string_matcher(text, next_state, m):
 			print(text[i-m+1:])
 
 if __name__ == '__main__':
-	temp_text    = "GTAACAGTAAACG"
+	text    = "GTAACAGTAAACG"
 	pattern = "AAC"
 
-	temp_next_state = {}
-	for i in range(len(set(temp_text))):
-		for char in set(temp_text):
-			temp_next_state[i, char] = 0
+	next_state = {}
+	for i in range(len(set(text))):
+		for char in set(text):
+			next_state[i, char] = 0
 
-	temp_next_state[0, "A"] = 1
+	next_state[0, "A"] = 1
 
-	temp_next_state[1, "A"] = 2
+	next_state[1, "A"] = 2
 
-	temp_next_state[2, "A"] = 2
-	temp_next_state[2, "C"] = 3
+	next_state[2, "A"] = 2
+	next_state[2, "C"] = 3
 
-	temp_next_state[3, "A"] = 1
+	next_state[3, "A"] = 1
 
-	fa_string_matcher(temp_text, temp_next_state, len(pattern))
+	print("Text:   ", text)
+	print("Pattern:", pattern)
+	print()
+	fa_string_matcher(text, next_state, len(pattern))
