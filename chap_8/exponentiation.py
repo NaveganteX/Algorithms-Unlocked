e1ba21a2b9b1f8c592bd4eb60b39bba6a542825f
Rahn <zaffrahn@gmail.com>
2016-06-12 23:07:38 +0800

    exponentiation.py modified

--- chap_8/exponentiation.py
+++ chap_8/exponentiation.py
@@ -3,7 +3,7 @@ def exponentiation(x, n):
 		if time is 0:
 			return accumulate
 		if time%2 is 0:
-			return _exp(base*base, int(time/2), accumulate)
+			return _exp(base**2, time//2, accumulate)
 		return _exp(base, time-1, base*accumulate)
 
 	return _exp(x, n, 1)
